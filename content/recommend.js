ecfb8ce6bcc3513ecc7320bbc9ed2c2b760c2085
nobodxbodon <nobodxbodon@1f528157-424a-4ce6-8bfe-5c2123bf455e>
2011-01-20 23:30:10 +0000

    dev only: load page event capture, dup still
    git-svn-id: https://fwtw.svn.sourceforge.net/svnroot/fwtw@170 1f528157-424a-4ce6-8bfe-5c2123bf455e

---
+++ content/recommend.js
@@ -0,0 +1,40 @@
+com.wuxuan.fromwheretowhere.recommendation = function(){
+  var pub={};
+  
+  var lasttitle = "";
+    
+  pub.onPageLoad = function(event){
+    // this is the content document of the loaded page.
+    var doc = event.originalTarget;
+    
+    //Application.storage.set("currentPage", doc.location.href);
+    if (doc.nodeName == "#document") {
+    //if (doc instanceof HTMLDocument) {
+      // is this an inner frame?
+      if (doc.defaultView.frameElement) {
+        // Frame within a tab was loaded.
+        // Find the root document:
+        com.wuxuan.fromwheretowhere.currentPage = doc;
+        while (doc.defaultView.frameElement) {
+          doc = doc.defaultView.frameElement.ownerDocument;
+        }
+        if(doc.title!=lasttitle){
+          /*var savenote = document.getElementById("current_title");
+          savenote.value = "title: "+doc.title;
+          document.getElementById("currentTitle").openPopup(null, "", 60, 50, false, false);*/
+          alert(doc.title);
+          lasttitle=doc.title;
+        }
+        //Application.storage.set("currentPage", doc.title);
+      }
+    }
+  };
+  
+  pub.init = function(){
+    //gbrowser.addEventListener("DOMContentLoaded", _onPageLoad, false);
+    window.addEventListener("DOMTitleChanged", pub.onPageLoad, false);
+    alert("init recommend");
+  };
+    
+  return pub;
+}();
