8186afb1a0fbdc34e16ac1bc7bccb47abd555278
nobodxbodon <nobodxbodon@1f528157-424a-4ce6-8bfe-5c2123bf455e>
2011-01-20 23:28:54 +0000

    BUG FIX 0.19.9: when there's no super node, null exception (query), should be null instead.
    git-svn-id: https://fwtw.svn.sourceforge.net/svnroot/fwtw@169 1f528157-424a-4ce6-8bfe-5c2123bf455e

--- content/main.js
+++ content/main.js
@@ -623,7 +623,7 @@ pub.mainThread.prototype = {
     //show the current url if no parents found
     if(nodes.length==0){
       if(pid){
-	nodes.push(pub.nodefromPlaceid(pid, query));
+	nodes.push(pub.nodefromPlaceid(pid, null));
       } else {
 	nodes.push(pub.ReferedHistoryNode(-1, -1, "No history found", null, false, false, [], 1));
       }
@@ -902,6 +902,7 @@ pub.mainThread.prototype = {
   };
 
   pub.search = function() {
+    //alert(Application.storage.get("currentPage", false));
     pub.treeView.treeBox.rowCountChanged(0, -pub.treeView.visibleData.length);
     pub.treeView.addSuspensionPoints(-1, -1);
     pub.keywords = document.getElementById("keywords").value;
