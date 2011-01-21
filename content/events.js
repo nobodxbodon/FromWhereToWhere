cd232ec2a0297e4108f15e0f2bcb5f47702fe93a
nobodxbodon <nobodxbodon@1f528157-424a-4ce6-8bfe-5c2123bf455e>
2011-01-21 05:14:06 +0000

    now time to actually build recommendation based on the page title separately, test in history first.
    git-svn-id: https://fwtw.svn.sourceforge.net/svnroot/fwtw@172 1f528157-424a-4ce6-8bfe-5c2123bf455e

---
+++ content/events.js
@@ -0,0 +1,49 @@
+com.wuxuan.fromwheretowhere.events = function(){
+  var pub={};
+  
+  var lasttitle = "";
+  var eventNum = 0;
+  
+  pub.onPageLoad = function(event){
+    eventNum +=1;
+    // this is the content document of the loaded page.
+    var doc = event.originalTarget;
+    
+    //Application.storage.set("currentPage", doc.location.href);
+    if (doc.nodeName == "#document") {
+    //if (doc instanceof HTMLDocument) {
+      // is this an inner frame?
+      if (doc.defaultView.frameElement) {
+        // Frame within a tab was loaded.
+        // Find the root document:
+        com.wuxuan.fromwheretowhere.currentPage = doc;
+        while (doc.defaultView.frameElement) {
+          doc = doc.defaultView.frameElement.ownerDocument;
+        }
+        var currentDoc = gBrowser.selectedBrowser.contentDocument;
+        if(currentDoc.title!=lasttitle){
+          lasttitle=currentDoc.title;
+          //alert(document.title);
+          //alert(currentDoc.title);
+          com.wuxuan.fromwheretowhere.recommendation.recommend(lasttitle);
+          //can't get from overlay, still wondering
+          /*var savenote = currentDoc.getElementById("current_title");
+          savenote.value = "title: "+currentDoc.title;
+          currentDoc.getElementById("currentTitle").openPopup(null, "", 60, 50, false, false);
+          */
+          //alert(eventNum + " "+doc.title + " " + lasttitle);
+          
+        }
+        //Application.storage.set("currentPage", doc.title);
+      }
+    }
+  };
+  
+  pub.init = function(){
+    window.addEventListener("DOMContentLoaded", pub.onPageLoad, false);
+    //window.addEventListener("DOMTitleChanged", pub.onPageLoad, false);
+    alert("init recommend");
+  };
+    
+  return pub;
+}();
--- content/recommend.js
+++ content/recommend.js
@@ -1,41 +1,22 @@
 com.wuxuan.fromwheretowhere.recommendation = function(){
   var pub={};
   
-  var lasttitle = "";
-  var eventNum = 0;
-  pub.onPageLoad = function(event){
-    eventNum +=1;
-    // this is the content document of the loaded page.
-    var doc = event.originalTarget;
-    
-    //Application.storage.set("currentPage", doc.location.href);
-    if (doc.nodeName == "#document") {
-    //if (doc instanceof HTMLDocument) {
-      // is this an inner frame?
-      if (doc.defaultView.frameElement) {
-        // Frame within a tab was loaded.
-        // Find the root document:
-        com.wuxuan.fromwheretowhere.currentPage = doc;
-        while (doc.defaultView.frameElement) {
-          doc = doc.defaultView.frameElement.ownerDocument;
-        }
-        if(doc.title!=lasttitle){
-          /*var savenote = document.getElementById("current_title");
-          savenote.value = "title: "+doc.title;
-          document.getElementById("currentTitle").openPopup(null, "", 60, 50, false, false);*/
-          lasttitle=doc.title;
-          alert(eventNum + " "+doc.title + " " + lasttitle);
-          
-        }
-        //Application.storage.set("currentPage", doc.title);
+  pub.recommend = function(title){
+    var stopwords = com.wuxuan.fromwheretowhere.corpus.stopwords_en_NLTK;
+    var allwords = title.split(" ");
+    alert(allwords);
+    for(var i=0; i<allwords.length; i++){
+      allwords[i] = allwords[i].toLowerCase();
+      if(stopwords.indexOf(allwords[i])>-1){
+        allwords.splice(i, 1);
+        i--;
       }
     }
+    alert(allwords);
   };
-  
+
   pub.init = function(){
-    window.addEventListener("DOMContentLoaded", pub.onPageLoad, false);
-    //window.addEventListener("DOMTitleChanged", pub.onPageLoad, false);
-    alert("init recommend");
+    
   };
     
   return pub;
