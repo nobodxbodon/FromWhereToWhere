59b8afab7bc51fa8af25f0299a7185dcea12b472
nobodxbodon <nobodxbodon@1f528157-424a-4ce6-8bfe-5c2123bf455e>
2011-05-26 03:09:41 +0000

    associate pref with toolbar button action: none/search/open_suggest
    git-svn-id: https://fwtw.svn.sourceforge.net/svnroot/fwtw@405 1f528157-424a-4ce6-8bfe-5c2123bf455e

--- content/openwindow.js
+++ content/openwindow.js
@@ -1,11 +1,26 @@
   
 com.wuxuan.fromwheretowhere.openWindow = function(){
   var pub={};
-    
+  
+  pub.PREF = Components.classes['@mozilla.org/preferences;1'].getService(Components.interfaces.nsIPrefBranch);
+  
   //get current tab url before open new tab and switch there!!
   pub.searchhistory = function(){
     Application.storage.set("currentURI", gBrowser.currentURI.spec);
     gBrowser.selectedTab = gBrowser.addTab("chrome://FromWhereToWhere/content/custtreeview.xul");
+  };
+  
+  pub.action = function(){
+    var act = pub.PREF.getIntPref("extensions.fromwheretowhere.buttonAction");
+    if(act==0){
+      //alert(act);
+    }
+    else if(act==1)
+      pub.searchhistory();
+    else if(act==2){
+      com.wuxuan.fromwheretowhere.recommendation.init();
+      com.wuxuan.fromwheretowhere.recommendation.popUp("","",[],[]);
+    }
   };
   
   return pub;
