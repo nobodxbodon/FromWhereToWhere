2c328ab9e9a3302d8172b6b051b6a51cb4803fa4
Xuan Wu <fromwheretowhere.service@gmail.com>
2012-09-06 21:22:56 -0700

    tests for 0.25.1rc1

---
+++ toRelease.sh
@@ -0,0 +1,11 @@
+cp chrome.manifest release/;
+cp install.rdf release/;
+cp content/*.xul release/content/;
+cp content/*.js release/content/;
+cp content/toolbar-button/*.css release/content/toolbar-button/;
+cp content/toolbar-button/*.png release/content/toolbar-button/;
+cp locale/en-US/*.dtd release/locale/en-US/;
+cp locale/sv-SE/*.dtd release/locale/sv-SE/;
+cp locale/zh-CN/*.dtd release/locale/zh-CN/;
+cp defaults/preferences/*.js release/defaults/preferences/;
+cp skin/*.css release/skin/;
